eb24be2111431ca348f855640e8e02821e8e05fb
Bursac Milan <bursac.m@bluewin.ch>
2022-03-10 19:23:05 +0100

    backend

---
+++ myfit_backend/MyFit_Backend/Models/BmiData.cs
@@ -0,0 +1,8 @@
+namespace MyFit_Backend.Models;
+
+public class BmiData
+{
+    public float Weight { get; set; }
+    public float Height { get; set; }
+    public double TimeStamp { get; set; }
+}
---
+++ myfit_backend/MyFit_Backend/Models/BmiResponse.cs
@@ -0,0 +1,7 @@
+namespace MyFit_Backend.Models;
+
+public class BmiResponse
+{
+    public float Bmi { get; set; }
+    public int TimeStamp { get; set; }
+}
